4366313b9da7adc60b76bcc11759dc488ee0c73e
Venisha-Pomelopay <venishachauhan1234@gmail.com>
2022-10-13 12:40:57 +0530

    Update create sqs code

--- AWS/sqs-practice/dist/sqs/createQueue.js
+++ AWS/sqs-practice/dist/sqs/createQueue.js
@@ -8,22 +8,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createQueue = void 0;
-const aws_sdk_1 = __importDefault(require("aws-sdk"));
+const sqsInstance_1 = require("../common/sqsInstance");
 const createQueue = (event, context, callback) => __awaiter(void 0, void 0, void 0, function* () {
-    aws_sdk_1.default.config.update({ region: "REGION" });
     // Create an SQS service object
-    const sqs = new aws_sdk_1.default.SQS({
-        apiVersion: "2012-11-05",
-        endpoint: "http://localhost:9324",
-        accessKeyId: "na",
-        secretAccessKey: "na",
-        region: "eu-west-1",
-    });
+    const sqs = (0, sqsInstance_1.sqsInstance)();
     var params = {
         QueueName: "demo-queue",
     };
@@ -35,37 +25,7 @@ const createQueue = (event, context, callback) => __awaiter(void 0, void 0, void
             console.log(data.QueueUrl);
         }
     });
-    // const params = {
-    //   // Remove DelaySeconds parameter and value for FIFO queues
-    //   DelaySeconds: 10,
-    //   MessageAttributes: {
-    //     Title: {
-    //       DataType: "String",
-    //       StringValue: "The Whistler",
-    //     },
-    //     Author: {
-    //       DataType: "String",
-    //       StringValue: "John Grisham",
-    //     },
-    //     WeeksOn: {
-    //       DataType: "Number",
-    //       StringValue: "6",
-    //     },
-    //   },
-    //   MessageBody:
-    //     "Information about current NY Times fiction bestseller for week of 12/11/2016.",
-    //   // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-    //   // MessageGroupId: "Group1",  // Required for FIFO queues
-    //   QueueUrl: "http://localhost:9324/sqs/DemoQueue"
-    // };
     const queues = yield sqs.listQueues().promise();
     console.log(queues);
-    // sqs.sendMessage(params, function (err, data) {
-    //   if (err) {
-    //     console.log("Error", err);
-    //   } else {
-    //     console.log("Success", data.MessageId);
-    //   }
-    // });
 });
 exports.createQueue = createQueue;
--- AWS/sqs-practice/src/sqs/createQueue.ts
+++ AWS/sqs-practice/src/sqs/createQueue.ts
@@ -1,16 +1,10 @@
 import AWS from "aws-sdk";
+import { sqsInstance } from "../common/sqsInstance";
 
 export const createQueue = async (event: any, context: any, callback: any) => {
-  AWS.config.update({ region: "REGION" });
 
   // Create an SQS service object
-  const sqs = new AWS.SQS({
-    apiVersion: "2012-11-05",
-    endpoint: "http://localhost:9324",
-    accessKeyId: "na",
-    secretAccessKey: "na",
-    region: "eu-west-1",
-  });
+  const sqs = sqsInstance()
 
   var params = {
     QueueName: "demo-queue",
@@ -23,38 +17,7 @@ export const createQueue = async (event: any, context: any, callback: any) => {
     }
   });
 
-  // const params = {
-  //   // Remove DelaySeconds parameter and value for FIFO queues
-  //   DelaySeconds: 10,
-  //   MessageAttributes: {
-  //     Title: {
-  //       DataType: "String",
-  //       StringValue: "The Whistler",
-  //     },
-  //     Author: {
-  //       DataType: "String",
-  //       StringValue: "John Grisham",
-  //     },
-  //     WeeksOn: {
-  //       DataType: "Number",
-  //       StringValue: "6",
-  //     },
-  //   },
-  //   MessageBody:
-  //     "Information about current NY Times fiction bestseller for week of 12/11/2016.",
-  //   // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-  //   // MessageGroupId: "Group1",  // Required for FIFO queues
-  //   QueueUrl: "http://localhost:9324/sqs/DemoQueue"
-  // };
-
   const queues = await sqs.listQueues().promise();
   console.log(queues);
 
-  // sqs.sendMessage(params, function (err, data) {
-  //   if (err) {
-  //     console.log("Error", err);
-  //   } else {
-  //     console.log("Success", data.MessageId);
-  //   }
-  // });
 };
